1ec6101249b4650bc9239e202e3aab495a85725b
wieczorek.dominika@gmail.com <wieczorek.dominika@gmail.com>
2024-04-05 12:23:13 +0200

    First javascript test

---
+++ test/first.test.js
@@ -0,0 +1,11 @@
+import request from "supertest";
+
+const baseUrl = "https://gorest.co.in/public/v2/";
+describe('get users', () => {
+    it('should return 200 status code',
+    async() => {
+        const response = await
+        request(baseUrl).get('users');
+        expect(response.statusCode).toBe(200);
+    });
+});
